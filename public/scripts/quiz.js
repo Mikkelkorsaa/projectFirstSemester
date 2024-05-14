4bc63175403b68eee492775455ba9443467322b2
jesperhe123 <jerichsen@ymail.com>
2024-05-14 13:03:54 +0200

    Nyquiz

--- public/scripts/quiz.js
+++ public/scripts/quiz.js
@@ -1,54 +1,15 @@
-var textElement = document.getElementById("changeable-text2");
-var buttonElement = document.getElementById("change-button2");
+var flipBoxInner = document.getElementById("flip-box-inner");
 
-buttonElement.addEventListener("click", function() {
+function toggleFlip() {
+  // Fjerner klikhændelseslytteren
+  flipBoxInner.removeEventListener("click", toggleFlip);
+  // Flipper boksen
+  flipBoxInner.style.transform = "rotateY(180deg)";
+}
 
-  textElement.innerHTML = "svar";
-});
-
-var changeableText1 = document.getElementById("changeable-text1");
-var changeButton1 = document.getElementById("change-button1");
-var changeableText2 = document.getElementById("changeable-text2");
-var changeButton2 = document.getElementById("change-button2");
-var changeableText3 = document.getElementById("changeable-text3");
-var changeButton3 = document.getElementById("change-button3");
-var changeableText4 = document.getElementById("changeable-text4");
-var changeButton4 = document.getElementById("change-button4");
-var changeableText5 = document.getElementById("changeable-text5");
-var changeButton5 = document.getElementById("change-button5");
-
-
-changeButton1.addEventListener("click", function() {
-
-  changeableText1.textContent = "Kul er mest farligt";
-  
-  changeButton1.textContent = "Skjul svar";
-});
-
-changeButton2.addEventListener("click", function() {
-
-  changeableText2.textContent = "Kul";
-  
-  changeButton2.textContent = "Skjul svar";
-});
-
-changeButton3.addEventListener("click", function() {
- 
-  changeableText3.textContent = "Kul dræber flest";
-  
-  changeButton3.textContent = "Skjul svar";
-});
-
-changeButton4.addEventListener("click", function() {
- 
-  changeableText4.textContent = "Nuclear";
-  
-  changeButton4.textContent = "Skjul svar";
-});
-
-changeButton5.addEventListener("click", function() {
-  
-  changeableText5.textContent = "Ja";
-  
-  changeButton5.textContent = "Skjul svar";
-});
+function revealAnswer(element) {
+  // Flipper boksen
+  toggleFlip();
+  // Viser svaret
+  document.getElementById("question-back").style.display = "block";
+}
