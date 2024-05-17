05f9e1dbb3a82c5f5d7fa4dc4091697430fc0631
Mikkel Korsaa <mikkelkorsaa@gmail.com>
2024-05-17 09:02:52 +0200

    Fixed things

--- server/queries.js
+++ server/queries.js
@@ -11,7 +11,7 @@ const pool = new Pool({
 });
 
 const insertData = (request, response) => {
-  const caldata = "../public/datasheets/powerplant_co2_and_deathrate.csv";
+  const caldata = "../public/datasheets/EG_FEC_RNEW.csv";
   const options = {
     delimiter: ','
   }
@@ -19,16 +19,37 @@ const insertData = (request, response) => {
   csvtojson().fromFile(caldata, options).then(source => {
     // Iterate through each row of the CSV data
     source.forEach(row => {
-      /* const { country, country_long, name, gppd_idnr, capacity_mw, latitude, longitude, primary_fuel, other_fuel1, other_fuel2, other_fuel3, commissioning_year, owner, url, geolocation_source, wepp_id, year_of_capacity_data, generation_gwh_2013, generation_gwh_2014, generation_gwh_2015, generation_gwh_2016, generation_gwh_2017, generation_gwh_2018, generation_gwh_2019, generation_data_source, estimated_generation_gwh_2013, estimated_generation_gwh_2014, estimated_generation_gwh_2015, estimated_generation_gwh_2016, estimated_generation_gwh_2017, estimated_generation_note_2013, estimated_generation_note_2014, estimated_generation_note_2015, estimated_generation_note_2016, estimated_generation_note_2017 } = row;
+      /* // For the tmp_power_plants
+      const { country, country_long, name, gppd_idnr, capacity_mw, latitude, longitude, primary_fuel, other_fuel1, other_fuel2, other_fuel3, commissioning_year, owner, url, geolocation_source, wepp_id, year_of_capacity_data, generation_gwh_2013, generation_gwh_2014, generation_gwh_2015, generation_gwh_2016, generation_gwh_2017, generation_gwh_2018, generation_gwh_2019, generation_data_source, estimated_generation_gwh_2013, estimated_generation_gwh_2014, estimated_generation_gwh_2015, estimated_generation_gwh_2016, estimated_generation_gwh_2017, estimated_generation_note_2013, estimated_generation_note_2014, estimated_generation_note_2015, estimated_generation_note_2016, estimated_generation_note_2017 } = row;
 
       const insertStatement = `INSERT INTO tmp_power_plants (country, country_long, name, gppd_idnr, capacity_mw, latitude, longitude, primary_fuel, other_fuel1, other_fuel2, other_fuel3, commissioning_year, owner, url, geolocation_source, wepp_id, year_of_capacity_data, generation_gwh_2013, generation_gwh_2014, generation_gwh_2015, generation_gwh_2016, generation_gwh_2017, generation_gwh_2018, generation_gwh_2019, generation_data_source, estimated_generation_gwh_2013, estimated_generation_gwh_2014, estimated_generation_gwh_2015, estimated_generation_gwh_2016, estimated_generation_gwh_2017, estimated_generation_note_2013, estimated_generation_note_2014, estimated_generation_note_2015, estimated_generation_note_2016, estimated_generation_note_2017) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18, $19, $20, $21, $22, $23, $24, $25, $26, $27, $28, $29, $30, $31, $32, $33, $34, $35)`;
       const items = [country, country_long, name, gppd_idnr, capacity_mw, latitude, longitude, primary_fuel, other_fuel1, other_fuel2, other_fuel3, commissioning_year, owner, url, geolocation_source, wepp_id, year_of_capacity_data, generation_gwh_2013, generation_gwh_2014, generation_gwh_2015, generation_gwh_2016, generation_gwh_2017, generation_gwh_2018, generation_gwh_2019, generation_data_source, estimated_generation_gwh_2013, estimated_generation_gwh_2014, estimated_generation_gwh_2015, estimated_generation_gwh_2016, estimated_generation_gwh_2017, estimated_generation_note_2013, estimated_generation_note_2014, estimated_generation_note_2015, estimated_generation_note_2016, estimated_generation_note_2017];
       */
 
+      /* For the fuel_info
       const { source, deathrate, co2_emission_in_tons } = row;
 
       const insertStatement = `INSERT INTO fuel_info (fuel_name,deathrate,co2_emission_in_tons) VALUES ($1, $2, $3)`;
-      const items = [source, deathrate, co2_emission_in_tons];
+      const items = [source, deathrate, co2_emission_in_tons]; 
+      */
+
+      const { Goal, Target, Indicator, SeriesCode, SeriesDescription, GeoAreaCode, GeoAreaName, ReportingType, Units, year_2000, year_2001, year_2002, year_2003, year_2004, year_2005, year_2006, year_2007, year_2008, year_2009, year_2010, year_2011, year_2012, year_2013, year_2014, year_2015, year_2016, year_2017, year_2018, year_2019, year_2020, year_2021 } = row;
+
+      const insertStatement = `INSERT INTO tmp_ren (
+      Goal, Target, Indicator, SeriesCode, SeriesDescription, GeoAreaCode, GeoAreaName, ReportingType, Units, 
+      year_2000, year_2001, year_2002, year_2003, year_2004, year_2005, year_2006, year_2007, year_2008, year_2009, 
+      year_2010, year_2011, year_2012, year_2013, year_2014, year_2015, year_2016, year_2017, year_2018, year_2019, 
+      year_2020, year_2021) 
+      VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18, $19, $20, $21, $22, $23, $24, $25, $26, $27, $28, $29, $30, $31)`;
+
+      const items = [
+        Goal, Target, Indicator, SeriesCode, SeriesDescription, GeoAreaCode, GeoAreaName, ReportingType, Units,
+        year_2000, year_2001, year_2002, year_2003, year_2004, year_2005, year_2006, year_2007, year_2008, year_2009,
+        year_2010, year_2011, year_2012, year_2013, year_2014, year_2015, year_2016, year_2017, year_2018, year_2019,
+        year_2020, year_2021
+      ];
+
+
 
 
       // Inserting data of current row into database
