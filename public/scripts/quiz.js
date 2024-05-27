d1fa7b01226abe59724dba708022faed87175c7a
Mikkel Korsaa <mikkelkorsaa@gmail.com>
2024-05-27 10:01:31 +0200

    Working quiz and some styling on the page

--- public/scripts/quiz.js
+++ public/scripts/quiz.js
@@ -1,68 +1,118 @@
-var currentQuestionIndex = 0;
-var isFlipped = false;
-
-var questions = [
-  { correctAnswer: "Coal", feedback: "Coal is the most dangerous" },
-  { correctAnswer: "Coal", feedback: "Coal emits the most Co2" },
-  { correctAnswer: "Oil", feedback: "Oil has the highest deathrate of the 3" },
-  { correctAnswer: "Nuclear", feedback: "Nuclear power plants" },
-  { correctAnswer: "Yes", feedback: "Nuclear power plants are the greenest" }
+const questions = [
+  {
+    question: "What is the most dangerous energy source?",
+    answers: ["Nuclear", "Coal", "Solar"],
+    feedback: "Coal power plants are the energy source that emits the most CO2 and is the most dangerous to be around!",
+    correctAnswer: "Coal"
+  },
+  {
+    question: "Which power plant emits the most CO2?",
+    answers: ["Coal", "Natural Gas", "Oil"],
+    feedback: "Coal is more carbon-intensive than burning natural gas or petroleum for electric power production!",
+    correctAnswer: "Coal"
+  },
+  {
+    question: "Which one kills the most?",
+    answers: ["Oil", "Gas", "Nuclear"],
+    feedback: "Oil has a death rate that is 613 times higher than Nuclear!",
+    correctAnswer: "Oil"
+  },
+  {
+    question: "Which energy source has the highest efficiency in energy production?",
+    answers: ["Coal", "Nuclear", "Solar"],
+    feedback: "Nuclear has the most efficiency in energy production.",
+    correctAnswer: "Nuclear"
+  }
 ];
 
-function toggleFlip() {
-  var flipBoxInner = document.getElementById("flip-box-inner");
-  if (isFlipped) {
-    flipBoxInner.style.transform = "rotateY(0deg)";
-    isFlipped = false;
+let index = 0;
+let correctAnswers = 0;
+
+const wrongAnswer = function () {
+  d3.select(this).style("background-color", "#ffcccc");
+  document.getElementById("quiz-response").textContent = "Wrong!";
+  document.getElementById("quiz-feedback").textContent = questions[index].feedback;
+  setTimeout(d => {
+    document.getElementById("flip-card-inner").style.transform = "rotateY(180deg)";
+  }, 500)
+  index++;
+}
+
+const correctAnswer = function () {
+  d3.select(this).style("background-color", "#ccffcc")
+  document.getElementById("quiz-response").textContent = "Correct!";
+  document.getElementById("quiz-feedback").textContent = questions[index].feedback;
+  setTimeout(d => {
+    document.getElementById("flip-card-inner").style.transform = "rotateY(180deg)";
+  }, 500)
+  index++;
+  correctAnswers++;
+}
+
+const startQuiz = function () {
+  d3.select(this).remove();
+  d3.selectAll("#quiz-description").text("")
+  document.getElementById("quiz-header").textContent = questions[index].question;
+  d3.select("#flip-card-front")
+    .append("button")
+    .attr("id", "answerOne")
+    .text(questions[index].answers[0])
+    .on("click", wrongAnswer);
+  d3.select("#flip-card-front")
+    .append("button")
+    .attr("id", "answerTwo")
+    .text(questions[index].answers[1])
+    .on("click", correctAnswer);
+  d3.select("#flip-card-front")
+    .append("button")
+    .attr("id", "answerThree")
+    .text(questions[index].answers[2])
+    .on("click", wrongAnswer);
+}
+
+const tryAgain = function () {
+  d3.selectAll("#restart-quiz").remove();
+  document.getElementById("quiz-header").textContent = "Test your knowlege of power plants!";
+  d3.select("#flip-card-back")
+    .append("button")
+    .attr("id", "next-question")
+    .text("Next Question")
+    .on("click", nextQuestion);
+  index = 0;
+  correctAnswers = 0;
+  startQuiz();
+}
+
+const nextQuestion = function () {
+  if (index < questions.length) {
+    document.getElementById("flip-card-inner").style.transform = "rotateY(0deg)";
+    d3.selectAll("#answerOne, #answerTwo, #answerThree").style("background-color", "#e7e7e7");
+    d3.select("#quiz-question").text(questions[index].question);
+    if (questions[index].answers[0] === questions[index].correctAnswer) {
+      d3.select("#answerOne").text(questions[index].answers[0]).on("click", correctAnswer);
+    } else {
+      d3.select("#answerOne").text(questions[index].answers[0]).on("click", wrongAnswer);
+    }
+    if (questions[index].answers[1] === questions[index].correctAnswer) {
+      d3.select("#answerTwo").text(questions[index].answers[1]).on("click", correctAnswer);
+    } else {
+      d3.select("#answerTwo").text(questions[index].answers[1]).on("click", wrongAnswer);
+    }
+    if (questions[index].answers[2] === questions[index].correctAnswer) {
+      d3.select("#answerThree").text(questions[index].answers[2]).on("click", correctAnswer);
+    } else {
+      d3.select("#answerThree").text(questions[index].answers[2]).on("click", wrongAnswer);
+    }
   } else {
-    flipBoxInner.style.transform = "rotateY(180deg)";
-    isFlipped = true;
-  }
-}
-
-function revealAnswer(element) {
-  var userAnswer = element.textContent;
-  var answerFeedback = document.querySelector("#question-back-" + currentQuestionIndex + " #answer-feedback");
-
-  // Check if the answer is correct
-  if (userAnswer === questions[currentQuestionIndex].correctAnswer) {
-    answerFeedback.textContent = "Correct Answer!";
-    element.classList.add("correct-answer");
-  } else {
-    answerFeedback.textContent = "Wrong Answer. The correct answer is: " + questions[currentQuestionIndex].correctAnswer;
-    element.classList.add("wrong-answer");
+    document.getElementById("flip-card-inner").style.transform = "rotateY(0deg)";
+    document.getElementById("quiz-header").textContent = "That's it!";
+    document.getElementById("quiz-description").textContent = "You got " + correctAnswers + "/4 correct!";
+    d3.select("#flip-card-inner").selectAll("button").remove();
+    d3.select("#flip-card-front").append("button").attr("id", "restart-quiz").text("Try again").on("click", tryAgain);
   }
 
-  // Display the answer feedback
-  document.getElementById("question-back-" + currentQuestionIndex).style.display = "block";
-
-  // Flip the box after a short delay
-  setTimeout(toggleFlip, 1000); // Adjust delay as needed
 }
 
-function nextQuestion() {
-  currentQuestionIndex++;
-  if (currentQuestionIndex < questions.length) { // Check against the number of questions
-    setTimeout(() => {
-      updateQuestion();
-      toggleFlip();
-    }, 500); // Adjust delay to ensure the flip animation completes
-  } else {
-    alert("Du har nået slutningen af quizzen!");
-  }
-}
+d3.select("#next-question").on("click", nextQuestion);
 
-function updateQuestion() {
-  var questionFrontOld = document.getElementById("question-front-" + (currentQuestionIndex - 1));
-  var questionBackOld = document.getElementById("question-back-" + (currentQuestionIndex - 1));
-
-  // Hide current question
-  if (questionFrontOld) questionFrontOld.style.display = "none";
-  if (questionBackOld) questionBackOld.style.display = "none";
-
-  // Display new question
-  var questionFrontNew = document.getElementById("question-front-" + currentQuestionIndex);
-  var questionBackNew = document.getElementById("question-back-" + currentQuestionIndex);
-  if (questionFrontNew) questionFrontNew.style.display = "block";
-  if (questionBackNew) questionBackNew.style.display = "block";
-}
+d3.select("#start-quiz").on("click", startQuiz);
--- sqlQueries/createTables.sql
+++ sqlQueries/createTables.sql
@@ -18,30 +18,3 @@ CREATE TABLE pin_info (
     country_id INTEGER REFERENCES country_info(country_id),
     fuel_id INTEGER REFERENCES fuel_info(fuel_id)
 );
-
-CREATE TABLE green_energy (
-    green_energy_id SERIAL PRIMARY KEY,
-    country_id INTEGER REFERENCES country_info(country_id),
-    year_2000 NUMERIC NOT NULL,
-    year_2001 NUMERIC NOT NULL,
-    year_2002 NUMERIC NOT NULL,
-    year_2003 NUMERIC NOT NULL,
-    year_2004 NUMERIC NOT NULL,
-    year_2005 NUMERIC NOT NULL,
-    year_2006 NUMERIC NOT NULL,
-    year_2007 NUMERIC NOT NULL,
-    year_2008 NUMERIC NOT NULL,
-    year_2009 NUMERIC NOT NULL,
-    year_2010 NUMERIC NOT NULL,
-    year_2011 NUMERIC NOT NULL,
-    year_2012 NUMERIC NOT NULL,
-    year_2013 NUMERIC NOT NULL,
-    year_2014 NUMERIC NOT NULL,
-    year_2015 NUMERIC NOT NULL,
-    year_2016 NUMERIC NOT NULL,
-    year_2017 NUMERIC NOT NULL,
-    year_2018 NUMERIC NOT NULL,
-    year_2019 NUMERIC NOT NULL,
-    year_2020 NUMERIC NOT NULL,
-    year_2021 NUMERIC NOT NULL
-);
